7f77857775db2020c860a43d54cfdee0e1973064
Muzamilali2017355 <73329018+Muzamilali2017355@users.noreply.github.com>
2020-11-22 19:13:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Muzamil Assignment 2
+            Muzamil Devops Assignment 2 
             <h3>CI and CD of react app on github</h3>
         </p>
         <a
